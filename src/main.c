bf30e76a98c472dd0c0e8e3ec3e52ac059c0d439
yhfudev <yhfudev@gmail.com>
2017-03-11 22:07:15 -0500

    update CI scripts.

--- src/main.c
+++ src/main.c
@@ -82,8 +82,9 @@ main(int argc, char * argv[])
     }
     if (ret < 0) {
         fprintf(stderr, "Error: in calculation!\n");
-        return -1;
+        exit(-1);
     }
     fprintf(stdout, "%s%s\n", show, buf);
+    exit (0);
     return 0;
 }
